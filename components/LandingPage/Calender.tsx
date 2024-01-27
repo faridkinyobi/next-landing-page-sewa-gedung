c2ce165806a89acf01dc8c97882d6af3d6f21af9
faridkinyobi <faridkin7@gmail.com>
2024-01-27 11:25:36 +0700

    memperbaiki tampilan modal dengan menambahkan table dan memperbaiki tampilan calender

--- components/LandingPage/Calender.tsx
+++ components/LandingPage/Calender.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import dynamic from 'next/dynamic';
-const Calendar = dynamic(() => import('react-calendar'), { ssr: false });
-import { BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
+import dynamic from "next/dynamic";
+const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
+import { BsCaretLeftFill, BsCaretRightFill, BsX } from "react-icons/bs";
 import { add, sub, format } from "date-fns";
 import CustomModal from "../modal";
 interface PaketType {
@@ -10,6 +10,9 @@ interface PaketType {
     kegiatan: string;
     tgl_mulai: Date;
     tgl_akhir: Date;
+    lama_sewa: number;
+    status_kegiatan: string;
+    waktu: number;
   }[];
 }
 const EventCalendar = ({ event }: PaketType) => {
@@ -17,57 +20,48 @@ const EventCalendar = ({ event }: PaketType) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [showModal, setShowModal] = useState(false);
 
-
-  const getEventForDate = () => {
-    const normalizedDate = new Date();
-    normalizedDate.setHours(0, 0, 0, 0);
-    
-    return event.filter((events) =>{
-      const kegiatan = events.kegiatan
-      const eventStartDate = new Date(events.tgl_mulai);
-      const eventEndDate = new Date(events.tgl_akhir)
-      
-      return normalizedDate >= eventStartDate&& normalizedDate <= eventEndDate &&kegiatan
-      }
-      );
-    };
-    const [selectedEvent, setSelectedEvent] = useState({});
-
-
-  const tileContent = ({ date,view }: { date: Date; view: string }) => {
+  const tileContent = ({ date, view }: { date: Date; view: string }) => {
     if (view === "month") {
       const event = getEventsForDate(date);
       if (event) {
-        return event.map((item,index)=>(
-          <div key={index} className="bg-white-10 w-[6rem] h-10 text-yellow-400">
-            <span className="text-white">{item.kegiatan}</span>
+        return event.map((item, index) => (
+          <div
+            key={index}
+            className="bg-red-500 mx-1  w-[3rem] md:w-[6rem] h-10 rounded-md p-1 hover:bg-red-900/100 delay-100  border border-red-900"
+          >
+            <p className="text text-[0.7rem] md:text-base text-blue-30 hover:text-slate-300/85 ">
+              {item.kegiatan}
+            </p>
           </div>
-        ))
+        ));
       }
     }
     return null;
   };
   const getEventsForDate = (date: Date) => {
-   // Atur komponen waktu dari tanggal saat ini ke tengah malam
+    // Atur komponen waktu dari tanggal saat ini ke tengah malam
     const normalizedDate = new Date(date);
     normalizedDate.setHours(0, 0, 0, 0);
-  
+
     return event.filter((e) => {
       //  Atur kondisi komponen waktu tanggal mulai dan akhir acara menjadi tengah malam
       const eventStartDate = new Date(e.tgl_mulai);
       eventStartDate.setHours(0, 0, 0, 0);
-  
+
       const eventEndDate = new Date(e.tgl_akhir);
       eventEndDate.setHours(0, 0, 0, 0);
-  
-      return normalizedDate.getTime() >= eventStartDate.getTime() && normalizedDate.getTime() <= eventEndDate.getTime();
+
+      return (
+        normalizedDate.getTime() >= eventStartDate.getTime() &&
+        normalizedDate.getTime() <= eventEndDate.getTime()
+      );
     });
   };
-  
+
   const handleDateClick = (date: Date) => {
     setSelectedDate(date);
     const eventsForDate = getEventsForDate(date);
-    if(eventsForDate.length > 0){
+    if (eventsForDate.length > 0) {
       setShowModal(!showModal);
     }
   };
@@ -76,8 +70,15 @@ const EventCalendar = ({ event }: PaketType) => {
     setShowModal(false);
   };
   return (
-    <section className="justify-center items-center mx-10 lg:mx-[6rem] my-5 lg:my-20">
-      <div className="nav-months p-2 bg-slate-400 flex justify-between">
+    <section className="justify-center items-center mx-10 lg:mx-[6rem] my-5 lg:my-20  ">
+      <h1 className=" font-bold text-3xl ml-4 text-blue-40 ">
+        Jadwal
+      </h1>
+      <p className="py-2 md:text-2xl text-xl  ml-4 text-blue-20">
+        Lihat jadwal pada calender dibawah
+      </p>
+      <div className="border-4 border-blue-30 rounded-xl">
+      <div className="nav-months p-2 bg-slate-400 flex justify-between rounded-t-lg">
         <p className=" font-light">{format(activeStartDate, "MMMM yyyy")}</p>
         <p className=" font-light text-base">
           {format(new Date(), "d MMMM yyyy")}
@@ -89,7 +90,7 @@ const EventCalendar = ({ event }: PaketType) => {
               setActiveStartDate(sub(activeStartDate, { months: 1 }))
             }
           >
-            <BsCaretLeftFill/>
+            <BsCaretLeftFill />
           </button>
           <button
             className=" bg-blue-40 text-white-10 px-3 lg:px-6 py-2 text-xl font-extrabold hover:bg-blue-30"
@@ -100,50 +101,86 @@ const EventCalendar = ({ event }: PaketType) => {
             <BsCaretRightFill />
           </button>
         </div>
-      </div>{" "}         
+      </div>
       <Calendar
-     activeStartDate={activeStartDate}
-     value={selectedDate}
-     onClickDay={handleDateClick}
-     tileContent={tileContent}
-     calendarType="gregory"
-     minDetail="year"
-     showNavigation={false}
-     className="text-[1rem] lg:text-2xl bg-blue-20 text-center lg:px-20 lg:py-10 px-2"
-   />
-      {/* <Calendar
         activeStartDate={activeStartDate}
-        // onChange={handleDateChange}
         value={selectedDate}
         onClickDay={handleDateClick}
         tileContent={tileContent}
         calendarType="gregory"
         minDetail="year"
         showNavigation={false}
-        className="text-[1rem] lg:text-2xl bg-blue-20 text-center lg:px-20 lg:py-10 px-2"
-      /> */}
-      <div className="py-10 px-5">
-        <h5 className="text-2xl text-amber-400 flex items-center">
-          <span className="w-10 h-10 bg-green-10 mr-6"></span>Sudah di pesan
+        className="text-[1rem] lg:text-2xl bg-blue-20  text-white-10/55 text-center lg:px-20 lg:py-10 px-2"
+      />
+      <div className="py-5 px-5">
+        <h5 className="text-sm md:text-xl text-amber-400 flex items-center">
+          <span className="w-5 h-5 md:w-6 md:h-6 bg-red-500 mr-6"></span>Sudah
+          di pesan
         </h5>
       </div>
       {/* Checkbox and Modal */}
-      <span> Tampilkan Modal</span>
       <CustomModal isOpen={showModal}>
-      {showModal && selectedDate && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            {/* <h2>Events on {selectedDate.toDateString()}</h2> */}
-            <ul>
-              {getEventsForDate(selectedDate).map((item,index) => (
-                <li key={index}>{format(new Date(item.tgl_akhir), "dd/MM/yyyy")}</li>
-              ))}
-            </ul>
-            <button onClick={handleCloseModal}>Close Modal</button>
+        {showModal && selectedDate && (
+          <div className="modal-overlay flex justify-center bg-slate-500 mx-4">
+            <div className="modal-content">
+              <table className="table w-[10rem]">
+                <thead className="">
+                  <tr className="m-1 text-center text-xs">
+                    <th className="border border-blue-20 px-[0.1rem] md:px-5">
+                      {" "}
+                      kegiatan
+                    </th>
+                    <th className="border border-blue-20 px-[0.1rem] md:px-5">
+                      lama sewa
+                    </th>
+                    <th className="border border-blue-20 px-[0.1rem] md:px-6">
+                      tanggal mulai
+                    </th>
+                    <th className="border border-blue-20 px-[0.2rem] md:px-6">
+                      tanggal akhir
+                    </th>
+                    <th className="border border-blue-20 px-[0.1rem] md:px-5">
+                      waktu sewa
+                    </th>
+                    <th className="border border-blue-20 px-[0.1rem] md:px-5">
+                      kegiatan
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {getEventsForDate(selectedDate).map((item, index) => (
+                    <tr className="static text-center" key={index}>
+                      <td className="border border-blue-20 ">
+                        {item.kegiatan}
+                      </td>
+                      <td className="border border-blue-20 ">
+                        {item.lama_sewa}
+                      </td>
+                      <td className="border border-blue-20 ">
+                        {format(new Date(item.tgl_mulai), "MM/dd/yyyy")}
+                      </td>
+                      <td className="border border-blue-20">
+                        {format(new Date(item.tgl_akhir), "MM/dd/yyyy")}
+                      </td>
+                      <td className="border border-blue-20 ">{item.waktu}</td>
+                      <td className="border border-blue-20 ">
+                        {item.status_kegiatan}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button
+                className="close absolute right-6 top-[15rem] md:top-[10rem] md:right-[16rem] md:m-5 font-medium text-3xl m-1 hover:bg-black-10/15 bg-slate-400 rounded-full"
+                onClick={handleCloseModal}
+              >
+                <BsX />
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </CustomModal>
+      </div>
     </section>
   );
 };
