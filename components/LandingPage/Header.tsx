551d1e21e3bce52f3e61bbc344385919e2a7e8ff
faridkinyobi <faridkin7@gmail.com>
2024-01-29 10:01:09 +0700

    perubahan pada pengkondisian clicked awal kondisi false ada transisi perpindahan mt sekarang konsisi tue baru ada perpindahan, perubahan pada fasilitas desc di desc

--- components/LandingPage/Header.tsx
+++ components/LandingPage/Header.tsx
@@ -2,26 +2,26 @@
 import React, { useState } from "react";
 import NavbarLending from "../NavbarLending";
 import Button from "../Button";
-import { BsPCircle, BsPeople } from "react-icons/bs";
+import { BsPCircle,BsFillPersonLinesFill,BsFillLuggageFill  } from "react-icons/bs";
 import { useRouter } from 'next/navigation'
  
 const Header = () => {
-  const [clicked, setClicked] = useState(true);
+  const [clicked, setClicked] = useState(false);
   const router = useRouter()
   return (
     <header
-      className="h-[60rem] lg:h-[59.125rem]   bg-cover bg-center"
-      style={{ backgroundImage: "url('backgroud3.svg')" }}
+      className="h-[60rem] lg:h-[59.125rem]  bg-cover bg-center"
+      style={{ backgroundImage: "url('/img/backgroud3.svg')" }}
     >
       <NavbarLending clicked={clicked} setClicked={setClicked} />
       <div
-        className={`text-center py-[2rem] lg:py-28 justify-center ${
+        className={`text-center py-10  lg:py-28 justify-center ${
           clicked
-            ? "transition_duration"
-            : "py-[8rem] mt-40 transition_duration lg:mt-auto"
+            ? "transition_duration py-[8.5rem] mt-40 transition_duration lg:mt-auto"
+            : "transition_duration"
         }`}
       >
-        <div className="text-white-10 font-semibold lg:text-[3rem] leading-none text-[45px] ">
+        <div className="titel text-white-10 font-semibold lg:text-[3rem] leading-none text-[2.5rem] ">
           Sukses{" "}
           <span className="bg-gradient-to-br from-blue-500 to-teal-400 bg-clip-text text-transparent">
             Acara Anda  
@@ -34,7 +34,7 @@ const Header = () => {
           Kami
         </div>
         <div className="flexCenter">
-          <p className="lg:w-[50%] sm:[50%] w-[90%] py-7 lg:py-8 text-gray-10 text-[1.3rem] lg:text-2xl font-light ">
+          <p className="desc lg:w-[50%] sm:[50%] w-[90%] py-7 lg:py-8 text-gray-10 text-[1.3rem] lg:text-2xl font-light ">
             Kami menyediakan tempat yang terbaik dan fasilitas yang bisa
             digunakan untuk memberikan kenyamanan dalam menyewa gedung
           </p>
@@ -53,29 +53,26 @@ const Header = () => {
           <BsPCircle className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml block" />
           <div className="bg-blue-30 rounded-2xl w-[14rem]  lg:w-[20rem] z-[-1] p-8  top-[2.6rem] absolute text-center  font-light  leading-relaxed">
             <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10 ">Parkiran </h1>
-            <p className="text text-base lg:text-xl text-gray-10 ">
-              parkiran luas bisa <br />
-              menampung banyak kendaraan
+            <p className="text font-light text-sm md:text-base text-gray-10 text-left ">
+              Parkiran luas bisa menampung banyak kendaraan mini bus,mobil, motor
             </p>
           </div>
         </div>
         <div className="flex justify-center mb-[12rem] lm:m-0 ">
-          <BsPCircle className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
+          <BsFillPersonLinesFill className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
           <div className="bg-blue-30 rounded-2xl w-[14rem] lg:w-[20rem] z-[-1] p-8  top-[19rem] md:top-[2.4rem] lg:top-[2.6rem] absolute text-center font-light  leading-relaxed">
           <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10  ">Kapasita</h1>
-            <p className="text text-base lg:text-xl text-gray-10 ">
-              Kapasitas gedung<br />
-              bisa menampung 2000 orang
+            <p className="text font-light text-sm md:text-base text-gray-10  text-left ">
+             Gedung dapat menampung 2000 orang dan bisa digunakan untuk berbagi event
             </p>
           </div>
         </div>
         <div className="flex justify-center  lm:m-0 ">
-          <BsPCircle className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
+          <BsFillLuggageFill  className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
           <div className="bg-blue-30 rounded-2xl w-[14rem]  lg:w-[20rem] z-[-1] p-8  top-[35.64rem] md:top-[2.6rem] absolute text-center font-light  leading-relaxed">
-          <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10">Parkiran </h1>
-            <p className="text text-base lg:text-xl text-gray-10">
-              parkiran luas bisa <br />
-              menampung banyak kendaraan
+          <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10">Ruang Rias</h1>
+            <p className="text font-light text-sm md:text-base text-gray-10  text-left">
+            Ruang khusus di mana pengantin dapat bersiap-siap sebelum acara utama
             </p>
           </div>
         </div>
