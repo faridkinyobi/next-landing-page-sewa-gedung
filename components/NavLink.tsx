642487270e49430598a04eb291a90036d8d41f66
faridkinyobi <faridkin7@gmail.com>
2024-01-30 18:23:20 +0700

    menghapus use client

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,8 +1,7 @@
-"use client";
+
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
-import { useEffect } from 'react';
 
 type NavLinkProps = {
   href: string;
@@ -16,7 +15,7 @@ const NavLink = ({ href, children,className}: NavLinkProps) => {
  
 
   return (
-    <Link href={href} className={` ${pathname === href ? 'active' : '' } ${className}`} >
+    <Link href={href} className={` ${pathname === href? 'active' : ''} ${className}`} >
       {children}
     </Link>
   );
