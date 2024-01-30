4e31bc4f2b1e084b645982c86e3650eeb80276be
faridkinyobi <faridkin7@gmail.com>
2024-01-30 20:50:20 +0700

    menambahkan GetServerSidePropsContext dan merubah kondisi useState true manjaid false dan merubah transition mt-10

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,10 +1,11 @@
 import React,{useState} from "react";
+import { GetServerSidePropsContext } from 'next';
 
 import NavbarLending from "@/components/NavbarLending";
 import FormSignin from "../components/FormSignin";
 
 export default function Signin() {
-  const [clicked, setClicked] = useState(true);
+  const [clicked, setClicked] = useState(false);
 
   return (
     <main className="bg-blue-30 lg:h-[50rem] h-[50rem] sm:h-[78rem]">
@@ -12,7 +13,7 @@ export default function Signin() {
       <div className="container flex items-center justify-center">
         <div
           className={`bg-white-20 lg:ml-24 rounded-2xl mt-10 sm:mt-[9rem] lg:mt-[2rem] ${
-            clicked ? "transition_duration" : " mt-56 transition_duration"
+            clicked ? "transition_duration mt-56" : "transition_duration"
           }`}
         >
           <div className="m-10">
@@ -24,7 +25,7 @@ export default function Signin() {
     </main>
   );
 }
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context:GetServerSidePropsContext ) {
   const { token } = context.req.cookies;
 
   if (token) {
