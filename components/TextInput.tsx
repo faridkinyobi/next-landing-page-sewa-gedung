9f07ca5a86a39529cc9381193d52a95f41865f18
faridkinyobi <faridkin7@gmail.com>
2024-01-30 18:16:04 +0700

    menambahkan {my} mergin top dan margin botton

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -17,10 +17,10 @@ const TextInput = ({
   label,
 }: TextInputProps) => {
   return (
-    <div className="flex flex-col items-start col-span-1">
+    <div className="flex flex-col items-start col-span-1 my-2">
       <label className="text-blue-30">{label}</label>
       <input
-      className="rounded-lg  focus:border-blue-20 focus:outline-none box-border border-2 border-gray-10 px-4 py-3 lg:w-96 my-2"
+      className="rounded-lg focus:border-blue-20 focus:outline-none box-border border-2 border-gray-10 px-4 py-3 lg:w-96 my-2"
         type={type}
         placeholder={placeholder}
         name={name}
