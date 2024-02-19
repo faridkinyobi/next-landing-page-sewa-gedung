58601d703518e0a606bc8a6cabab09157d528d6f
faridkinyobi <faridkin7@gmail.com>
2024-02-19 20:55:48 +0700

    memperbaiki style Fasilitas, menambahkan objek,menghapus usestate click

--- components/LandingPage/Header.tsx
+++ components/LandingPage/Header.tsx
@@ -1,30 +1,48 @@
-
 import React, { useState } from "react";
 import NavbarLending from "../NavbarLending";
 import Button from "../Button";
-import { BsPCircle,BsFillPersonLinesFill,BsFillLuggageFill  } from "react-icons/bs";
-import { useRouter } from 'next/navigation'
- 
+import {
+  BsPCircle,
+  BsFillPersonLinesFill,
+  BsFillLuggageFill,
+} from "react-icons/bs";
+import { useRouter } from "next/navigation";
+const Faslitem = [
+  {
+    titel: "Parkiran",
+    desc: "Parkiran luas bisa menampung banyak kendaraan mini bus,mobil, motor",
+    icon: (
+      <BsPCircle className=" mx-auto text-white-10 text-6xl md:text-7xl bg-blue-30 p-2 rounded-full shadow-ml md:mt-[-4.30rem] " />
+    ),
+  },
+  {
+    titel: "Kapasita",
+    desc: "Gedung dengan berkapasitas 2000 orang, cocok untuk berbagai event",
+    icon: (
+      <BsFillPersonLinesFill className=" mx-auto text-white-10 text-6xl md:text-7xl bg-blue-30 p-2 rounded-full shadow-ml md:mt-[-4.30rem]" />
+    ),
+  },
+  {
+    titel: "Ruang Rias",
+    desc: "Ruang khusus di mana pengantin dapat bersiap-siap sebelum acara utama",
+    icon: (
+      <BsFillLuggageFill className=" mx-auto text-white-10 text-6xl md:text-7xl bg-blue-30 p-2 rounded-full shadow-ml md:mt-[-4.30rem]" />
+    ),
+  },
+];
 const Header = () => {
-  const [clicked, setClicked] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
   return (
     <header
-      className="h-[60rem] lg:h-[59.125rem]  bg-cover bg-center"
+      className="h-[60rem] md:h-[59.125rem]  bg-cover bg-center"
       style={{ backgroundImage: "url('/img/backgroud3.svg')" }}
     >
-      <NavbarLending clicked={clicked} setClicked={setClicked} />
-      <div
-        className={`text-center py-10  lg:py-28 justify-center ${
-          clicked
-            ? "transition_duration py-[8.5rem] mt-40 transition_duration lg:mt-auto"
-            : "transition_duration"
-        }`}
-      >
-        <div className="titel text-white-10 font-semibold lg:text-[3rem] leading-none text-[2.5rem] ">
+      <NavbarLending />
+      <div className={`text-center mt-24 md:mt-24 lg:mt-20 justify-center`}>
+        <h1 className="titel text-white-10 font-semibold md:text-[3rem] leading-none text-[2.5rem] ">
           Sukses{" "}
           <span className="bg-gradient-to-br from-blue-500 to-teal-400 bg-clip-text text-transparent">
-            Acara Anda  
+            Acara Anda
           </span>
           <br />
           Di{" "}
@@ -32,50 +50,39 @@ const Header = () => {
             Gedung
           </span>{" "}
           Kami
-        </div>
+        </h1>
         <div className="flexCenter">
-          <p className="desc lg:w-[50%] sm:[50%] w-[90%] py-7 lg:py-8 text-gray-10 text-[1.3rem] lg:text-2xl font-light ">
+          <p className="desc md:w-[70%] lg:w-[50%] w-[90%] my-5 mx-10 lg:py-8 text-gray-10 text-[1.3rem] md:text-2xl font-light text-left md:text-center">
             Kami menyediakan tempat yang terbaik dan fasilitas yang bisa
             digunakan untuk memberikan kenyamanan dalam menyewa gedung
           </p>
         </div>
         <Button
-          className="btn_green w-32 xl:py-3 py-3  mt-7 shadow-md shadow-green-10/50  duration-300 transition-all ease-in-out outline-2 active:outline focus:outline-gray-300 "
+          className="btn_green mt-7 px-14  xl:py-3 py-3 text-xl shadow-md shadow-green-10/50"
           type="button"
           title="Orders"
-          onClick={() => router.push('#Package')}
+          onClick={() => router.push("#Package")}
         />
       </div>
 
       {/* fasil */}
-      <div className="sm:flex  relative flex-row md:gap-[11rem] lg:gap-80 justify-center md:mt-96 lg:mt-[15rem] mt-[16rem] z-0">
-        <div className="flex justify-center mb-[12rem] lm:m-0">
-          <BsPCircle className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml block" />
-          <div className="bg-blue-30 rounded-2xl w-[14rem]  lg:w-[20rem] z-[-1] p-8  top-[2.6rem] absolute text-center  font-light  leading-relaxed">
-            <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10 ">Parkiran </h1>
-            <p className="text font-light text-sm md:text-base text-gray-10 text-left ">
-              Parkiran luas bisa menampung banyak kendaraan mini bus,mobil, motor
-            </p>
+      <div className="flex  relative flex-wrap md:gap-12 gap-10 justify-center  mt-[17rem] lg:mt-[19rem]">
+        {Faslitem.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center md:block bg-blue-30 text-center p-5 md:p-8 rounded-2xl"
+          >
+            {item.icon}
+            <div className="text-left mx-5">
+              <h1 className="font-bold text-xl md:text-lg lg:text-xl text-white-10 my-2 md:text-center">
+                {item.titel}
+              </h1>
+              <p className="text font-light text-sm md:text-base text-gray-10 text-left w-52 md:w-56  ">
+                {item.desc}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center mb-[12rem] lm:m-0 ">
-          <BsFillPersonLinesFill className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
-          <div className="bg-blue-30 rounded-2xl w-[14rem] lg:w-[20rem] z-[-1] p-8  top-[19rem] md:top-[2.4rem] lg:top-[2.6rem] absolute text-center font-light  leading-relaxed">
-          <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10  ">Kapasita</h1>
-            <p className="text font-light text-sm md:text-base text-gray-10  text-left ">
-             Gedung dapat menampung 2000 orang dan bisa digunakan untuk berbagi event
-            </p>
-          </div>
-        </div>
-        <div className="flex justify-center  lm:m-0 ">
-          <BsFillLuggageFill  className=" text-center text-white-10 text-7xl bg-blue-30 p-2 rounded-full shadow-ml" />
-          <div className="bg-blue-30 rounded-2xl w-[14rem]  lg:w-[20rem] z-[-1] p-8  top-[35.64rem] md:top-[2.6rem] absolute text-center font-light  leading-relaxed">
-          <h1 className="font-bold p-2 text-xl md:text-lg lg:text-xl text-white-10">Ruang Rias</h1>
-            <p className="text font-light text-sm md:text-base text-gray-10  text-left">
-            Ruang khusus di mana pengantin dapat bersiap-siap sebelum acara utama
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </header>
   );
