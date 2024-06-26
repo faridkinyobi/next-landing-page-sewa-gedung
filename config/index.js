3d794d8fd84c962bd95d5fc1a513204267dfac8f
kinyobi <113663696+faridkinyobi@users.noreply.github.com>
2024-06-26 12:18:53 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import errorHandler from './errorHandler.js';
-
+console.log(process.env.NEXT_PUBLIC_API)
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
 });
