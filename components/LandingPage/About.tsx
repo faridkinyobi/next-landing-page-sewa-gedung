03659bcceacc77a220bb777e36770167ebb78462
faridkinyobi <faridkin7@gmail.com>
2024-07-22 22:44:11 +0700

    update waktu setTimeout

--- components/LandingPage/About.tsx
+++ components/LandingPage/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
   const handleNavigation = async () => {
     setLoading(true);
     //  penundaan menampilkan 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     setLoading(false);
   };
   return (
